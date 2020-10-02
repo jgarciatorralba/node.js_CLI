85acab9998a70dd3d3320bef76aaaececf978985
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-02 15:26:49 +0200

    command 'get-persons' tested

--- moviedb.js
+++ moviedb.js
@@ -6,5 +6,19 @@ const { program } = require("commander");
 // Add some basic info such as version and short description
 program.version("1.0.0").description("Command Line Interface for TMDb API");
 
-// Test that CLI actually works
+// Implement command 'get-persons'
+program
+  .command("get-persons")
+  .description("Make a network request to fetch the most popular persons")
+  .requiredOption("-p, --popular", "Fetch the popular persons")
+  .requiredOption(
+    "--page <page>",
+    "The page of persons data results to fetch",
+    1 // Default value set to page 1
+  )
+  .action((command) => {
+    console.info(`Page ${command.page} selected!`);
+  });
+
+// Catch all arguments passed by the command line
 program.parse(process.argv);
