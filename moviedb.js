c4d15a075eaae962e4d5438b05b36413066dd820
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-02 13:40:23 +0200

    Added shebang to convert file to executable

--- moviedb.js
+++ moviedb.js
@@ -1,12 +1,10 @@
+#!/usr/bin/env node
+
 // Bring in 'commander' dependency
-const {
-  program
-} = require('commander');
+const { program } = require("commander");
 
 // Add some basic info such as version and short description
-program
-  .version('1.0.0')
-  .description('Command Line Interface for TMDb API');
+program.version("1.0.0").description("Command Line Interface for TMDb API");
 
 // Test that CLI actually works
 program.parse(process.argv);
