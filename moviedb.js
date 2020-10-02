7b2e8f8207999a44bfadb01b184eb61de1803c11
beron <bleink@outlook.es>
2020-10-02 12:49:11 +0200

    new command ins

--- moviedb.js
+++ moviedb.js
@@ -8,5 +8,15 @@ program
   .version('1.0.0')
   .description('Command Line Interface for TMDb API');
 
+// Some other commands
+program
+  .command('inspiration')
+  .description('gives you a inspirational phrase')
+  .alias('ins')
+  .action(function(word='default'){
+    if(word=='defaullt')console.log('you are configurable, want to program ?')
+    else console.log('words have a value, how deep can you see it ?')
+  })
+
 // Test that CLI actually works
 program.parse(process.argv);
