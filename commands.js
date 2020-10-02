e6ab8b2053f0d5ab69cae1656aa43944a9228328
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-02 11:07:30 +0200

    Added 'commands.js' file where the CLI instance is created

---
+++ commands.js
@@ -0,0 +1,12 @@
+// Bring in 'commander' dependency
+const {
+  program
+} = require('commander');
+
+// Add some basic info such as version and short description
+program
+  .version('1.0.0')
+  .description('Command Line Interface for TMDb API');
+
+// Test that CLI actually works
+program.parse(process.argv);
