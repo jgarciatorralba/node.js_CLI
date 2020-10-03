0538d16fa52b3d1a188decc782c1ac6aef31f0da
beron <bleink@outlook.es>
2020-10-03 22:56:52 +0200

    get-person

--- moviedb.js
+++ moviedb.js
@@ -120,5 +120,46 @@ program
       });
   });
 
+  //------------------------------Person details------------------------------------------
+  program.command('get-person')
+    .description('Make a network request to fetch the data of a single person')
+    .requiredOption('i, --id <num>', 'The id of the person', )
+    .action((cli)=>{
+      const chalk = require('chalk')
+      const ora = require('ora')
+      const spinner = ora(chalk.yellowBright('Fetching the person data...')).start()
+      const https = require('https')
+      require('dotenv/config')
+      // const url = 'https://api.themoviedb.org/3/person/' + cli.id + '?api_key=1a20d213f4f948df52672a96ed05965a'
+      const url = 'https://api.themoviedb.org/3/person/' + cli.id + '?api_key=' + process.env.API_KEY
+      https
+        .get(url, (resp)=>{
+          let data
+          resp.on('data', (d)=>{
+            data = d
+          })
+          resp.on('end', ()=>{
+            const dataObj = JSON.parse(data)
+            if(dataObj.success == false){
+              spinner.warn(chalk.bgYellow(chalk.black(dataObj.status_message)))
+            }else{
+              console.log(chalk.white('\n----------------------------------------'))
+              console.log(chalk.white('Person: \n'))
+              console.log(chalk.white('ID: '+dataObj.id))
+              console.log(chalk.white('Name: ')+chalk.green(dataObj.name))
+              console.log(chalk.white('Birthday: ' + dataObj.birthday + ' | ' + dataObj.place_of_birth))
+              console.log(chalk.white('Department: ') + chalk.redBright(dataObj.known_for_department))
+              console.log(chalk.green('Biography: '+dataObj.biography))
+              console.log(chalk.white('Also known as: \n'))
+              console.log(chalk.white(dataObj.also_known_as[0]))
+              spinner.succeed(chalk.bgGreen('Person data loaded !'))
+            }
+          })
+        })
+        .on("error", (e)=>{
+          spinner.fail(chalk.bgRed(chalk.black('oh no somethings wrong, that is going on here ?')))
+          console.log(chalk)
+        })
+    })
 // Catch all arguments passed by the command line
 program.parse(process.argv);
