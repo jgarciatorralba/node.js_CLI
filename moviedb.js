978c8444ac712e0ad68aa66d48a5c05d83702cc7
beron <bleink@outlook.es>
2020-10-04 20:55:42 +0200

    get movies

--- moviedb.js
+++ moviedb.js
@@ -89,10 +89,10 @@ program
               if (noMoviesWithTitle) {
                 console.info(
                   "\n" +
-                    chalk.redBright(
-                      `${person.name} doesn't appear in any movie`
-                    ) +
-                    "\n"
+                  chalk.redBright(
+                    `${person.name} doesn't appear in any movie`
+                  ) +
+                  "\n"
                 );
               } else {
                 console.info(chalk.white("\nAppearing in movies:"));
@@ -120,46 +120,95 @@ program
       });
   });
 
-  //------------------------------Person details------------------------------------------
-  program.command('get-person')
-    .description('Make a network request to fetch the data of a single person')
-    .requiredOption('i, --id <num>', 'The id of the person', )
-    .action((cli)=>{
-      const chalk = require('chalk')
-      const ora = require('ora')
-      const spinner = ora(chalk.yellowBright('Fetching the person data...')).start()
-      const https = require('https')
-      require('dotenv/config')
-      // const url = 'https://api.themoviedb.org/3/person/' + cli.id + '?api_key=1a20d213f4f948df52672a96ed05965a'
-      const url = 'https://api.themoviedb.org/3/person/' + cli.id + '?api_key=' + process.env.API_KEY
-      https
-        .get(url, (resp)=>{
-          let data
-          resp.on('data', (d)=>{
-            data = d
-          })
-          resp.on('end', ()=>{
-            const dataObj = JSON.parse(data)
-            if(dataObj.success == false){
-              spinner.warn(chalk.bgYellow(chalk.black(dataObj.status_message)))
-            }else{
-              console.log(chalk.white('\n----------------------------------------'))
-              console.log(chalk.white('Person: \n'))
-              console.log(chalk.white('ID: '+dataObj.id))
-              console.log(chalk.white('Name: ')+chalk.green(dataObj.name))
-              console.log(chalk.white('Birthday: ' + dataObj.birthday + ' | ' + dataObj.place_of_birth))
-              console.log(chalk.white('Department: ') + chalk.redBright(dataObj.known_for_department))
-              console.log(chalk.green('Biography: '+dataObj.biography))
-              console.log(chalk.white('Also known as: \n'))
-              console.log(chalk.white(dataObj.also_known_as[0]))
-              spinner.succeed(chalk.bgGreen('Person data loaded !'))
-            }
-          })
+//------------------------------Person details------------------------------------------
+program.command('get-person')
+  .description('Make a network request to fetch the data of a single person')
+  .requiredOption('i, --id <num>', 'The id of the person',)
+  .action((cli) => {
+    const chalk = require('chalk')
+    const ora = require('ora')
+    const spinner = ora(chalk.yellowBright('Fetching the person data...')).start()
+    const https = require('https')
+    require('dotenv/config')
+    // const url = 'https://api.themoviedb.org/3/person/' + cli.id + '?api_key=1a20d213f4f948df52672a96ed05965a'
+    const url = 'https://api.themoviedb.org/3/person/' + cli.id + '?api_key=' + process.env.API_KEY
+    https
+      .get(url, (resp) => {
+        let data = ""
+        resp.on('data', (d) => {
+          data += d
         })
-        .on("error", (e)=>{
-          spinner.fail(chalk.bgRed(chalk.black('oh no somethings wrong, that is going on here ?')))
-          console.log(chalk)
+        resp.on('end', () => {
+          const dataObj = JSON.parse(data)
+          if (dataObj.success == false) {
+            spinner.warn(chalk.bgYellow(chalk.black(dataObj.status_message)))
+          } else {
+            console.log(chalk.white('\n----------------------------------------'))
+            console.log(chalk.white('Person: \n'))
+            console.log(chalk.white('ID: ' + dataObj.id))
+            console.log(chalk.white('Name: ') + chalk.green(dataObj.name))
+            console.log(chalk.white('Birthday: ' + dataObj.birthday + ' | ' + dataObj.place_of_birth))
+            console.log(chalk.white('Department: ') + chalk.redBright(dataObj.known_for_department))
+            console.log(chalk.green('Biography: ' + dataObj.biography))
+            console.log(chalk.white('Also known as: \n'))
+            console.log(chalk.white(dataObj.also_known_as[0]))
+            spinner.succeed(chalk.bgGreen('Person data loaded !'))
+          }
         })
-    })
-// Catch all arguments passed by the command line
-program.parse(process.argv);
+      })
+      .on("error", (e) => {
+        spinner.fail(chalk.bgRed(chalk.black('oh no somethings wrong, that is going on here ?')))
+        console.log(chalk)
+      })
+  })
+program
+  .command('get-movies')
+  .description('Make a network request to fetch movies')
+  .requiredOption('--page <number>', 'The page of movies data results to fetch')
+  .option('-p, --popular', 'Fetch the popular movies')
+  .option('-n, --now-playing', 'Fetch the movies that are playing now')
+  .action((cli)=>{
+    const ora = require('ora')
+    const spinner = ora('Fetching the movies data...\n').start()
+    const chalk = require('chalk')
+    require('dotenv/config')
+    const options = {
+      hostname: 'api.themoviedb.org',
+      port: 443,
+      path: '/3/movie/popular?page='+cli.page+'&api_key='+process.env.API_KEY,
+      method: 'GET'
+    }
+    let message = 'Popular movies data loaded'
+    // let url = 'https://api.themoviedb.org/3/movie/popular?page='+cli.page+'&api_key='+process.env.API_KEY
+    if(cli.nowPlaying){
+      // url = 'https://api.themoviedb.org/3/movie/now_playing?page='+cli.page+'&api_key='+process.env.API_KEY
+      options.path = '/3/movie/now_playing?page='+cli.page+'&api_key='+process.env.API_KEY
+      message = 'Movies playing now data loaded'
+    }
+    const https = require('https')
+    let data = ""
+    https.request(options, req=>{
+      req.on('data', (res)=>{
+        data += res
+      })
+      req.on('end', ()=>{
+        const dataObj = JSON.parse(data)
+        dataObj.results.forEach(movie=>{
+          console.log(chalk.white('Movie: \n'))
+          console.log(chalk.white('ID: ' + movie.id))
+          console.log(chalk.white('Title: ') + chalk.blue(movie.title))
+          console.log(chalk.white(`Relese Date: ${movie.release_date}
+          
+          
+          `))
+        })
+        if(dataObj.total_pages > dataObj.page){
+          console.log(chalk.white('----------------------------------------'))
+          console.log(chalk.white('Page: ' + dataObj.page + " of: " + dataObj.total_pages + '\n'))
+        }
+        spinner.succeed(chalk.bgGreen.black(message))
+      })
+    }).end()
+  })
+  // Catch all arguments passed by the command line
+  program.parse(process.argv);
