441df3cf96bda7ab9690a4576f48bd672b383b07
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-05 14:05:21 +0200

    Modified 'get-persons' to include '--save' functionality

--- commands/get-persons.js
+++ commands/get-persons.js
@@ -3,6 +3,7 @@ require("dotenv/config");
 const ora = require("ora");
 const chalk = require("chalk");
 const https = require("https");
+const save_local = require("./../functions/save-local");
 
 // Export command as a function
 module.exports = function addGetPersonsCommand(program) {
@@ -21,6 +22,10 @@ module.exports = function addGetPersonsCommand(program) {
       let page = command.page;
       // Bring in the API Key from '.env' file
       let apiKey = process.env.API_KEY;
+      // Get '--save' and '--local' flag
+      let saveFlag = program.save;
+      let localFlag = program.local;
+
       // Base URL
       let url = "https://api.themoviedb.org/3/person/";
       // Chain to url the search for popular persons
@@ -51,66 +56,70 @@ module.exports = function addGetPersonsCommand(program) {
                 spinner.warn(error + "\n");
               })
             } else {
-              let currentPg = parsedData.page;
-              let totalPgs = parsedData.total_pages;
-              let persons = parsedData.results;
-              // Page number
-              if (totalPgs > currentPg) {
-                console.info(
-                  chalk.white("\n----------------------------------------")
-                );
-                console.info(
-                  chalk.white("Page " + currentPg + " of " + totalPgs)
-                );
-              }
-              // Persons data
-              persons.forEach((person) => {
-                console.info(
-                  chalk.white("----------------------------------------\n")
-                );
-                console.info(chalk.white("Person:\n"));
-                console.info(chalk.white("ID: " + person.id));
-                console.info(`Name: ${chalk.blue.bold(person.name)}`);
-                if (person.known_for_department === "Acting") {
+              if (saveFlag == true) {
+                save_local.saveFile(data, spinner, "persons");
+              } else {
+                let currentPg = parsedData.page;
+                let totalPgs = parsedData.total_pages;
+                let persons = parsedData.results;
+                // Page number
+                if (totalPgs > currentPg) {
                   console.info(
-                    `Department: ${chalk.magenta(person.known_for_department)}`
+                    chalk.white("\n----------------------------------------")
+                  );
+                  console.info(
+                    chalk.white("Page " + currentPg + " of " + totalPgs)
                   );
                 }
-                // Movies data
-                let movies = person.known_for;
-                let noMoviesWithTitle = true;
-                movies.forEach((movie) => {
-                  if (movie.title !== undefined) {
-                    noMoviesWithTitle = false;
-                  }
-                });
-                if (noMoviesWithTitle) {
+                // Persons data
+                persons.forEach((person) => {
                   console.info(
-                    "\n" +
-                    chalk.redBright(
-                      `${person.name} doesn't appear in any movie`
-                    ) +
-                    "\n"
+                    chalk.white("----------------------------------------\n")
                   );
-                } else {
-                  console.info(chalk.white("\nAppearing in movies:"));
+                  console.info(chalk.white("Person:\n"));
+                  console.info(chalk.white("ID: " + person.id));
+                  console.info(`Name: ${chalk.blue.bold(person.name)}`);
+                  if (person.known_for_department === "Acting") {
+                    console.info(
+                      `Department: ${chalk.magenta(person.known_for_department)}`
+                    );
+                  }
+                  // Movies data
+                  let movies = person.known_for;
+                  let noMoviesWithTitle = true;
                   movies.forEach((movie) => {
                     if (movie.title !== undefined) {
-                      console.info(chalk.white("\n\tMovie:"));
-                      console.info(chalk.white("\tID: " + movie.id));
-                      console.info(
-                        chalk.white("\tRelease date: " + movie.release_date)
-                      );
-                      console.info(
-                        chalk.white("\tTitle: " + movie.title + "\n")
-                      );
+                      noMoviesWithTitle = false;
                     }
                   });
-                }
-              });
-              // Ending spinner
-              console.info(chalk.white("\n"));
-              spinner.succeed(chalk.white("Popular Persons data loaded\n"));
+                  if (noMoviesWithTitle) {
+                    console.info(
+                      "\n" +
+                      chalk.redBright(
+                        `${person.name} doesn't appear in any movie`
+                      ) +
+                      "\n"
+                    );
+                  } else {
+                    console.info(chalk.white("\nAppearing in movies:"));
+                    movies.forEach((movie) => {
+                      if (movie.title !== undefined) {
+                        console.info(chalk.white("\n\tMovie:"));
+                        console.info(chalk.white("\tID: " + movie.id));
+                        console.info(
+                          chalk.white("\tRelease date: " + movie.release_date)
+                        );
+                        console.info(
+                          chalk.white("\tTitle: " + movie.title + "\n")
+                        );
+                      }
+                    });
+                  }
+                });
+                // Ending spinner
+                console.info(chalk.white("\n"));
+                spinner.succeed(chalk.white("Popular Persons data loaded\n"));
+              }
             }
           });
         })
