1bf081521b7ef5acb9d21818c8b2bf2f567e37ce
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-05 18:12:32 +0200

    Completed function 'loadFile' to use with '--local' flag

--- utils/local.js
+++ utils/local.js
@@ -2,3 +2,34 @@
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+
+function loadFile(spinner, typeOfData, flagMovies = "popular") {
+  let filesPath = path.resolve(process.cwd(), "files");
+  let personsPath = path.resolve(filesPath, "persons");
+  let moviesPath = path.resolve(filesPath, "movies");
+
+  let file_path = "";
+  if (typeOfData == "persons") {
+    file_path = path.resolve(personsPath, "popular-persons.json");
+  } else if (typeOfData == "movies") {
+    if (flagMovies == "popular") {
+      file_path = path.resolve(moviesPath, "popular-movies.json");
+    } else if (flagMovies == "now-playing") {
+      file_path = path.resolve(moviesPath, "now-playing-movies.json");
+    }
+  }
+
+  fs.readFile(file_path, (error, data) => {
+    if (error) {
+      spinner.fail(
+        chalk.bold.bgRed("Error: ") + chalk.bgRed(error.message + "\n")
+      );
+    } else {
+      let parsedData = JSON.parse(data)
+      const prints = require("../utils/prints");
+      prints.printGetPersons(spinner, parsedData);
+    }
+  })
+}
+
+exports.loadFile = loadFile;
