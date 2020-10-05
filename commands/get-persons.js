4c7a4c7a1eb6b740cba1f66a99fb363398ca003d
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-05 18:11:58 +0200

    Added 'if' statement to handle the file loading due to flag '--local'

--- commands/get-persons.js
+++ commands/get-persons.js
@@ -20,7 +20,7 @@ module.exports = function addGetPersonsCommand(program) {
       let page = command.page;
       // Bring in the API Key from '.env' file
       let apiKey = process.env.API_KEY;
-      // Get '--save' and '--local' flag
+      // Get '--save' and '--local' flag values
       let saveFlag = program.save;
       let localFlag = program.local;
       // Start spinner
@@ -31,50 +31,55 @@ module.exports = function addGetPersonsCommand(program) {
       spinner.color = "yellowBright";
       spinner.spinner = "triangle";
       spinner.start();
-      // Create 'options' object before using 'https.request'
-      const options = {
-        protocol: 'https:',
-        hostname: 'api.themoviedb.org',
-        port: 443,
-        path: '/3/person/popular?page=' + page + '&api_key=' + apiKey,
-        method: 'GET'
-      };
-      // Make http request
-      const https = require("https");
-      const req = https
-        .request(options, (resp) => {
-          let data = "";
-          // A chunk of data has been recieved
-          resp.on("data", (chunk) => {
-            data += chunk;
-          });
-          // The whole response has been received. Print out the result
-          resp.on("end", () => {
-            let parsedData = JSON.parse(data);
-            if (parsedData.errors !== undefined) {
-              parsedData.errors.forEach(error => {
-                spinner.warn(error + "\n");
-              })
-            } else {
-              if (saveFlag == true) {
-                const save = require("../utils/save");
-                save.saveFile(data, spinner, "persons");
+      if (localFlag == true) {
+        // Try to load file from local folder (ignoring other command options)
+        const local = require("../utils/local");
+        local.loadFile(spinner, "persons");
+      } else {
+        // Create 'options' object before using 'https.request'
+        const options = {
+          protocol: 'https:',
+          hostname: 'api.themoviedb.org',
+          port: 443,
+          path: '/3/person/popular?page=' + page + '&api_key=' + apiKey,
+          method: 'GET'
+        };
+        // Make http request
+        const https = require("https");
+        const req = https
+          .request(options, (resp) => {
+            let data = "";
+            // A chunk of data has been recieved
+            resp.on("data", (chunk) => {
+              data += chunk;
+            });
+            // The whole response has been received. Print out the result
+            resp.on("end", () => {
+              let parsedData = JSON.parse(data);
+              if (parsedData.errors !== undefined) {
+                parsedData.errors.forEach(error => {
+                  spinner.warn(error + "\n");
+                })
               } else {
-                const prints = require("../utils/prints");
-                prints.printGetPersons(parsedData);
-                // Ending spinner
-                console.info(chalk.white("\n"));
-                spinner.succeed(chalk.white("Popular Persons data loaded\n"));
+                if (saveFlag == true) {
+                  // Save request to local folder
+                  const save = require("../utils/save");
+                  save.saveFile(data, spinner, "persons");
+                } else {
+                  // Print request data
+                  const prints = require("../utils/prints");
+                  prints.printGetPersons(spinner, parsedData);
+                }
               }
-            }
+            });
+          })
+          .on("error", (e) => {
+            // Handle http error with 'ora.fail()'
+            spinner.fail(
+              chalk.bold.bgRed("Error: ") + chalk.bgRed(e.message + "\n")
+            );
           });
-        })
-        .on("error", (e) => {
-          // Handle http error with 'ora.fail()'
-          spinner.fail(
-            chalk.bold.bgRed("Error: ") + chalk.bgRed(e.message + "\n")
-          );
-        });
-      req.end();
+        req.end();
+      }
     });
 };
