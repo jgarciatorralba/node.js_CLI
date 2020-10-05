fce3fff5c0948ed60f5a24d302f82c73ebc49d11
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-05 09:35:15 +0200

    Added new file 'get-person' to implement command on separate file

---
+++ commands/get-person.js
@@ -0,0 +1,11 @@
+// Bring in dependencies: 'dotenv', 'ora', 'chalk' and 'http'
+require("dotenv/config");
+const ora = require("ora");
+const chalk = require("chalk");
+const https = require("https");
+
+// Export command as a function
+module.exports = function addGetPersonCommand(program) {
+  // Implement command 'get-person'
+
+}
