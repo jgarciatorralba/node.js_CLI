33f33041dcdf0b3ebb5f59a1ed6e05e0d57256f8
beron <bleink@outlook.es>
2020-10-05 16:51:27 +0200

    added review to get-movie

--- moviedb.js
+++ moviedb.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const chalk = require("chalk");
 // Bring in 'commander' dependency
 const { program } = require("commander");
 
@@ -214,66 +215,103 @@ program
   .command('get-movie')
   .description('Make a network request to fetch data of a single movie')
   .requiredOption('-i, --id <id>', 'The id of the movie')
-  .option('-r, -reviews', 'Fetch the reviews of the movie')
+  .option('-r, --reviews', 'Fetch the reviews of the movie')
   .action(cli=>{
-    const ora = require('ora')
-    const spinner = ora('Fetching the movie data...\n')
-    spinner.spinner = ('unicorn')
-    spinner.color = ('yellow')
-    spinner.start()
     require('dotenv/config')
-    let url = 'https://api.themoviedb.org/3/movie/' + cli.id
     if(cli.reviews){
-      url = 'https://api.themoviedb.org/3/movie/' + cli.id + '/reviews'
+      showReviews(cli)
+    }else{
+      showMovie(cli)
     }
-    url += '?api_key=' + process.env.API_KEY
-    const options = new URL(url)
-    const https = require('https')
-    https.request(options, (req)=>{
-      let data = ''
-      const chalk = require('chalk')
-      req.on('data', (d)=>{
-        data += d
-      })
-      req.on('error', err=>{
-        spinner.fail(err)
-      })
-      req.on('end', ()=>{
-        const dataObj = JSON.parse(data)
-        if(dataObj.success == false){
-          spinner.warn(dataObj.status_message)
-        }else{
-          console.log('------------------------------------------------------------\n\n')
-          console.log(chalk.white('ID: ' +dataObj.id))
-          console.log(chalk.white('Title :') + chalk.blue(dataObj.title))
-          console.log(chalk.white('Release Date :' +dataObj.release_date))
-          console.log(chalk.white('Runtime :' +dataObj.runtime))
-          console.log(chalk.white('Vote Count :' +dataObj.vote_count))
-          console.log(chalk.white('Overview :' +dataObj.overview + '\n\n'))
-          console.log(chalk.white('Genres :\n'))
-          if(dataObj.genres.length < 0){
-            console.log(chalk.yellow('The movie does not have a declared genre'))
-          }else{
-            dataObj.genres.forEach(genre => {
-              console.log(chalk.white(genre.name))
+    function showReviews(cli){
+      const url = 'https://api.themoviedb.org/3/movie/' + cli.id + '/reviews?api_key=' + process.env.API_KEY
+      const ora = require('ora')
+      const spinner = ora('Fetching the movie data...\n')
+      spinner.spinner = ('unicorn')
+      spinner.color = ('yellow')
+      spinner.start()
+      const options = new URL(url)
+      const https = require('https')
+      
+      https.request(options, (req)=>{
+        let data = ''
+        req.on('data', d => {
+          data += d
+        })
+        req.on('end', () => {
+          const dataObj = JSON.parse(data)
+          const results = dataObj.results
+          if(results.length>0){
+            console.log('\n')
+            if(dataObj.page > dataObj.total_pages){
+              console.log(chalk.white('\n--------------------------------------------'))
+              console.log(chalk.white('Page: ' + dataObj.page + ' from: ' + dataObj.total_pages))
+            }
+            results.forEach(review => {
+              console.log(chalk.white('Author: ') + chalk.blueBright(review.author))
+              console.log(chalk.white('Content: ' + review.content))
             })
           }
-          console.log(chalk.white('\n'))
-          if(dataObj.spoken_languages.length < 0){
-            console.log(chalk.yellow('The movie ' + dataObj.title + ' does not have a declared genre'))
+          console.log('\n')
+          spinner.succeed('Reviews')
+        })
+      }).end()
+    }
+    function showMovie(cli){
+      const url = 'https://api.themoviedb.org/3/movie/' + cli.id + '?api_key=' + process.env.API_KEY
+      const ora = require('ora')
+      const spinner = ora('Fetching the movie data...\n')
+      spinner.spinner = ('unicorn')
+      spinner.color = ('yellow')
+      spinner.start()
+      const options = new URL(url)
+      const https = require('https')
+      
+      https.request(options, (req)=>{
+        let data = ''
+        const chalk = require('chalk')
+        req.on('data', (d)=>{
+          data += d
+        })
+        req.on('error', err=>{
+          spinner.fail(err)
+        })
+        req.on('end', ()=>{
+          const dataObj = JSON.parse(data)
+          if(dataObj.success == false){
+            spinner.warn(dataObj.status_message)
           }else{
-            dataObj.spoken_languages.forEach(language => {
-              console.log(chalk.white(language.name))
-            })
+            console.log('------------------------------------------------------------\n\n')
+            console.log(chalk.white('ID: ' +dataObj.id))
+            console.log(chalk.white('Title :') + chalk.blue(dataObj.title))
+            console.log(chalk.white('Release Date :' +dataObj.release_date))
+            console.log(chalk.white('Runtime :' +dataObj.runtime))
+            console.log(chalk.white('Vote Count :' +dataObj.vote_count))
+            console.log(chalk.white('Overview :' +dataObj.overview + '\n\n'))
+            console.log(chalk.white('Genres :\n'))
+            if(dataObj.genres.length < 0){
+              console.log(chalk.yellow('The movie does not have a declared genre'))
+            }else{
+              dataObj.genres.forEach(genre => {
+                console.log(chalk.white(genre.name))
+              })
+            }
+            console.log(chalk.white('\n'))
+            if(dataObj.spoken_languages.length < 0){
+              console.log(chalk.yellow('The movie ' + dataObj.title + ' does not have a declared genre'))
+            }else{
+              dataObj.spoken_languages.forEach(language => {
+                console.log(chalk.white(language.name))
+              })
+            }
+            console.log(chalk.white('\n'))
+            // console.log(dataObj)
+            spinner.succeed('Movie data loaded !')
           }
-          console.log(chalk.white('\n'))
-          // console.log(dataObj)
-          spinner.succeed('Movie data loaded !')
-        }
+        })
       })
-    })
-    .end()
-    
+      .end()
+    }
   })
   // Catch all arguments passed by the command line
   program.parse(process.argv);
