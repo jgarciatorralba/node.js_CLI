cffea6f8d575f5dbbdb749db46723d060d9ab310
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-05 14:03:44 +0200

    Added two new options '--save' and '--local' in main file 'moviedb.js'

--- moviedb.js
+++ moviedb.js
@@ -8,7 +8,9 @@ const {
 // Add some basic info such as version and short description
 program
   .version("1.0.0")
-  .description("Command Line Interface for TMDb API");
+  .description("Command Line Interface for TMDb API")
+  .option("--save", "Store the contents in a json file inside a files folder")
+  .option("--local", "Read the data of the persons or the movies from the local file system instead of making a network request");
 
 // Import commands
 require("./commands/get-persons")(program);
