99287691d22be429410ab4d01a1a35be238ccf05
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-05 16:41:46 +0200

    Added file 'prints.js' to implement printing functionalities separately

---
+++ utils/prints.js
@@ -0,0 +1,75 @@
+const chalk = require("chalk");
+
+function printGetPersons(data) {
+  let currentPg = data.page;
+  let totalPgs = data.total_pages;
+  let persons = data.results;
+  // Page number
+  if (totalPgs > currentPg) {
+    console.info(
+      chalk.white("\n----------------------------------------")
+    );
+    console.info(
+      chalk.white("Page " + currentPg + " of " + totalPgs)
+    );
+  }
+  // Persons data
+  persons.forEach((person) => {
+    console.info(
+      chalk.white("----------------------------------------\n")
+    );
+    console.info(chalk.white("Person:\n"));
+    console.info(chalk.white("ID: " + person.id));
+    console.info(`Name: ${chalk.blue.bold(person.name)}`);
+    if (person.known_for_department === "Acting") {
+      console.info(
+        `Department: ${chalk.magenta(person.known_for_department)}`
+      );
+    }
+    // Movies data
+    let movies = person.known_for;
+    let noMoviesWithTitle = true;
+    movies.forEach((movie) => {
+      if (movie.title !== undefined) {
+        noMoviesWithTitle = false;
+      }
+    });
+    if (noMoviesWithTitle) {
+      console.info(
+        "\n" +
+        chalk.redBright(
+          `${person.name} doesn't appear in any movie`
+        ) +
+        "\n"
+      );
+    } else {
+      console.info(chalk.white("\nAppearing in movies:"));
+      movies.forEach((movie) => {
+        if (movie.title !== undefined) {
+          console.info(chalk.white("\n\tMovie:"));
+          console.info(chalk.white("\tID: " + movie.id));
+          console.info(
+            chalk.white("\tRelease date: " + movie.release_date)
+          );
+          console.info(
+            chalk.white("\tTitle: " + movie.title + "\n")
+          );
+        }
+      });
+    }
+  });
+}
+
+function printGetPerson(data) {
+
+}
+
+function printGetMovies(data) {
+
+}
+
+function printGetMovie(data) {
+
+}
+
+exports.printGetPersons = printGetPersons;
