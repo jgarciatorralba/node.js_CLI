86a4996b8fda6698218f849f8bcd2c9431cf1211
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-05 18:54:47 +0200

    Swiped order of parameters in function 'saveFile'

--- utils/save.js
+++ utils/save.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
-function saveFile(data, spinner, typeOfData, flagMovies = "popular") {
+function saveFile(spinner, data, typeOfData, flagMovies = "popular") {
   let filesPath = path.resolve(process.cwd(), "files");
   let folder_path = "";
   let file_path = "";
