14559085ce4330cce5a13f02d2f4e9a6669bfe1b
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-06 12:35:38 +0200

    Added new file for command 'get-movie'

---
+++ commands/get-movie.js
@@ -0,0 +1,12 @@
+// Bring in dependencies: 'dotenv', 'ora' and 'chalk'
+require("dotenv/config");
+const ora = require("ora");
+const chalk = require("chalk");
+
+// Export command as a function
+module.exports = function addGetMoviesCommand(program) {
+  // Implement command 'get-movie'
+
+
+
+}
